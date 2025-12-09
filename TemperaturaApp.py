70def1406568a96ccffe11deffc7a40e57d788ac
a01613318-hub <a01613318@tec.mx>
2025-12-09 15:34:04 -0600

    Update TemperaturaApp.py

--- TemperaturaApp.py
+++ TemperaturaApp.py
@@ -33,9 +33,7 @@ y = datos["AverageTemperature"]
 
 model = LinearRegression()
 model.fit(X, y)
-_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.30, random_state=1613318
-
+_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=1613318
 )
 
 prediccion = model.predict(df)[0]
