b06094d516926dcefc5d8224821ddfbfc20c1812
a01613318-hub <a01613318@tec.mx>
2025-12-09 15:43:09 -0600

    Update TemperaturaApp.py

--- TemperaturaApp.py
+++ TemperaturaApp.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 st.write("Predicción de temperatura")
